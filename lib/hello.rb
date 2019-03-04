448115ff4c635084ffa40c3d41e4d41ab8ae9393
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 16:12:27 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,14 @@
 names = ["Tim", "Tom", "Jim"]
 
 def hello_t(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i += 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
   end
-  return array
+    return array
 end
 
 hello_t(names) {|name| if name.start_with?("T")
